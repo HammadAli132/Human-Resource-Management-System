69a3320f1a4a3c5f6713b312a363c38ededcde16
HammadAli132 <108983041+HammadAli132@users.noreply.github.com>
2024-08-11 01:48:00 +0500

    Employee dashboard sidebar completed

---
+++ frontend/employee.js
@@ -0,0 +1,15 @@
+const profileBtn = document.getElementById("profile");
+const leaveBtn = document.getElementById("leave-request");
+const viewLeaveBtn = document.getElementById("view-leaves");
+
+profileBtn.addEventListener('click', () => {
+    alert("View Profile");
+});
+
+leaveBtn.addEventListener('click', () => {
+    alert("Request Leave");
+});
+
+viewLeaveBtn.addEventListener('click', () => {
+    alert("View Leaves");
+});
--- frontend/employeeDashboard.php
+++ frontend/employeeDashboard.php
@@ -1,5 +1,8 @@
 <?php
     session_start();
+    if (empty($_SESSION["username"])) {
+        header("location: ./signIn.html");
+    }
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +12,7 @@
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Dashboard | HRMS</title>
     <link rel='stylesheet' href='./style.css'>
+    <script src="./employee.js" defer></script>
 </head>
 <body>
     <header>
@@ -22,6 +26,20 @@
         </nav>
     </header>
     <main>
+        <div class="sideBar">
+            <p id="user-fullname">
+                <?php echo "{$_SESSION['first_Name']} {$_SESSION['last_Name']}"?>
+            </p>
+            <p id="role">
+                <?php echo ucfirst($_SESSION['role_Name']) ?>
+            </p>
+            <hr>
+            <ul>
+                <li id="profile" class="function">My Profile</li>
+                <li id="leave-request" class="function">Request Leave</li>
+                <li id="view-leaves" class="function">My Leaves</li>
+            </ul>
+        </div>
         
     </main>
 </body>
